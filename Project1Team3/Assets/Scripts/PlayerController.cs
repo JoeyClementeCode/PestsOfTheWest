81644d0a13188bd8deee56d72ef216c61bcd06f9
JoeyClementeCode <joewclemente@gmail.com>
2023-01-23 00:12:04 -0800

    Initial Movement

--- Project1Team3/Assets/Scripts/PlayerController.cs
+++ Project1Team3/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,21 @@ namespace pest
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private float speed = 5.0f;
+        private Rigidbody2D rigidBody;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            rigidBody = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-        
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            rigidBody.velocity += new Vector2(horizontalInput * speed * Time.deltaTime, rigidBody.velocity.y);
         }
     }
 }
