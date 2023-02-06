aeef14ff2e039d9ab3290746e100356696a367a2
JoeyClementeCode <joewclemente@gmail.com>
2023-02-05 19:00:26 -0800

    Pushed First Set of Bugs/Polish Changes
    - Fixed Player Direction Problem
    - Added a despawn time for pickaxes
    - Fixed Ground Detection for ramps

--- Project1Team3/Assets/Scripts/PickaxeFunction.cs
+++ Project1Team3/Assets/Scripts/PickaxeFunction.cs
@@ -7,6 +7,11 @@ namespace pest
 {
     public class PickaxeFunction : MonoBehaviour
     {
+        private void Start()
+        {
+            Destroy(gameObject, 4.0f);
+        }
+
         private void OnCollisionEnter2D(Collision2D col)
         {
             if (col.gameObject.CompareTag("Throwable"))
--- Project1Team3/Assets/Scripts/PlayerController.cs
+++ Project1Team3/Assets/Scripts/PlayerController.cs
@@ -39,7 +39,7 @@ namespace pest
             {
                 transform.localScale = new Vector3(-1, 1, 1);
             }
-            else
+            else if (horizontalInput > 0)
             {
                 transform.localScale = new Vector3(1, 1, 1);
             }
